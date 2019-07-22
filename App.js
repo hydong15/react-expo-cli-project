67e5f6682aecf1e98ee4ed58ba6ddb5b594d05a7
hgYim <hydong15@ajou.ac.kr>
2019-07-22 23:07:48 +0900

    Add button

--- App.js
+++ App.js
@@ -1,21 +1,15 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={[styles.textBig, styles.textBlue]}>Hello, world!</Text>
+      <Button title={'click me!'} onPress={() => console.log('Clicked!')}/>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  textBig: {
-    fontSize: 40,
-  },
-  textBlue: {
-    color: 'blue',
-  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
