85bf869a6b58f8ec4cae782c8d92720d9c3da948
hgYim <hydong15@ajou.ac.kr>
2019-07-23 00:20:38 +0900

    Pass touched item to handler

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FlatList, StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text, TouchableOpacity } from 'react-native';
 import { Constants } from 'expo';
 
 export default class App extends React.Component {
@@ -21,12 +21,16 @@ export default class App extends React.Component {
         });
       });
   }
+
+  onPressCity(item) {
+    console.log('onPressCity =', item);
+  }
   
   renderItem(city) {
     return (
-      <View style={styles.item}>
+      <TouchableOpacity style={styles.item} onPress={() => this.onPressCity(city)}>
         <Text style={styles.text}>{city}</Text>
-      </View>
+      </TouchableOpacity>
     );
   }
 
