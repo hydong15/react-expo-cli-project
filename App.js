ec5d8afd011f2866fc58903c852d9fce0eb6901c
hgYim <hydong15@ajou.ac.kr>
2019-07-22 23:34:51 +0900

    Modify text container

--- App.js
+++ App.js
@@ -1,43 +1,13 @@
 import React from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
-
-class CounterButton extends React.Component {
-  static defaultProps = {
-    counter: -1,
-  };
-
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      counter: props.counter,
-    };
-  }
-
-  clickHandler = () => {
-    this.setState({
-      counter: this.state.counter + 1,
-    });
-  };
-
-  render() {
-    return (
-      <View style={styles.buttonContainer}>
-        <Text>{this.state.counter}</Text>
-        <Button title={'click me!'} onPress={this.clickHandler}/>
-      </View>
-    );
-  }
-}
+import { StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <CounterButton counter={0}/>
-        <CounterButton count={1}/>
-        <CounterButton counter={2}/>
-        <CounterButton counter={3}/>
+        <Text style={styles.text}>1</Text>
+        <Text style={[styles.text, styles.text2]}>2</Text>
+        <Text style={styles.text}>3</Text>
       </View>
     );
   }
@@ -47,10 +17,21 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    alignItems: 'stretch',
     justifyContent: 'center',
   },
-  buttonContainer: {
-    width: '100%',
+
+  text: {
+    flex: 1,
+    borderWidth: 1,
+    borderColor: 'red',
+    textAlign: 'center',
+    textAlignVertical: 'center',
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+
+  text2: {
+    flex: 2,
   },
 });
