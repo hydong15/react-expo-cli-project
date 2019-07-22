b21df1f890a929261739c6ad49e562cb378f5e53
hgYim <hydong15@ajou.ac.kr>
2019-07-23 00:36:51 +0900

    Add title to navigation bar

--- CityList.js
+++ CityList.js
@@ -3,25 +3,29 @@ import { FlatList, StyleSheet, Text, TouchableOpacity } from 'react-native';
 import { Constants } from 'expo';
 
 export default class CityList extends React.Component {
+  static navigationOptions = {
+    title: 'Cities',
+  };
+  
   constructor(props) {
     super(props);
-
+    
     this.state = {
       cities: [],
     };
   }
-
+  
   componentDidMount() {
     fetch('http://demo6468405.mockable.io/weather-crawlers/cities')
-      .then(response => response.json())
-      .then(cities => {
-        console.log('cities =', cities.length);
-        this.setState({
-          cities
-        });
+    .then(response => response.json())
+    .then(cities => {
+      console.log('cities =', cities.length);
+      this.setState({
+        cities
       });
+    });
   }
-
+  
   onPressCity(item) {
     console.log('onPressCity =', item);
   }
@@ -29,40 +33,40 @@ export default class CityList extends React.Component {
   renderItem(city) {
     return (
       <TouchableOpacity style={styles.item} onPress={() => this.onPressCity(city)}>
-        <Text style={styles.text}>{city}</Text>
+      <Text style={styles.text}>{city}</Text>
       </TouchableOpacity>
-    );
-  }
-
-  render() {
-    return (
-      <FlatList style={styles.container}
-                renderItem={({item}) => this.renderItem(item)}
-                keyExtractor={item => item}
-                data={this.state.cities}
-      />
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    marginTop: Constants.statusBarHeight,
-  },
-
-  item: {
-    flex: 1,
-    height: 50,
-    justifyContent: 'center',
+      );
+    }
     
-    borderColor: 'orange',
-    borderWidth: 1,
-  },
-
-  text: {
-    fontSize: 20,
-    textAlign: 'center',
-  },
-});
+    render() {
+      return (
+        <FlatList style={styles.container}
+        renderItem={({item}) => this.renderItem(item)}
+        keyExtractor={item => item}
+        data={this.state.cities}
+        />
+        );
+      }
+    }
+    
+    const styles = StyleSheet.create({
+      container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        marginTop: Constants.statusBarHeight,
+      },
+      
+      item: {
+        flex: 1,
+        height: 50,
+        justifyContent: 'center',
+        
+        borderColor: 'orange',
+        borderWidth: 1,
+      },
+      
+      text: {
+        fontSize: 20,
+        textAlign: 'center',
+      },
+    });
