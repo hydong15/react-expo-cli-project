73ade7d256da3b7c25b42e7ec6882d5911d06097
hgYim <hydong15@ajou.ac.kr>
2019-07-22 18:04:17 +0900

    Practice in class

--- App.js
+++ App.js
@@ -4,12 +4,17 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Hi!</Text>
+      <Text style={styles.text}>Hello, world!</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  text: {
+    fontSize: 40,
+    color: 'blue',
+  },  
   container: {
     flex: 1,
     backgroundColor: '#fff',
